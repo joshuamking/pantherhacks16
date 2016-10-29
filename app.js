7440f4f50559b999aedfd05951342e965edc3507
Joshua King <joshua@antechdevelopment.com>
2016-10-29 18:56:06 -0400

    Externalized more js

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+function initMap() {
+	if (navigator.geolocation) {
+		navigator.geolocation
+				 .getCurrentPosition(showPosition,
+									 showPosition({
+													  coords: {
+														  latitude: 40.7127405,
+														  longitude: -74.0059379
+													  }
+												  }));
+	} else {
+		showPosition({
+						 coords: {
+							 latitude: 40.7127405,
+							 longitude: -74.0059379
+						 }
+					 }
+		);
+	}
+}
+
+function showPosition(position) {
+	new google.maps.Map(document.getElementById('map'), {
+		center: { lat: position.coords.latitude, lng: position.coords.longitude},
+		zoom: 12
+	});
+}
+
 window.onload = function () {
 
 };
